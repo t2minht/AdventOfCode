87926a58a1329705caea2a2406f0057c55b80290
t2minht <tuongtran0240@gmail.com>
2022-12-03 13:46:17 -0600

    Day 3 Part 2 Completed

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -1,13 +1,28 @@
 file = open("Day3Input.txt")
 count = 0
 letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lines = []
 for line in file:
-    mid = int(len(line)/2)
-    front = line[0:mid]
-    back = line[mid:-1]
-    for c in front:
-        if back.find(c) != -1:
-            print(front + " " + back + ": " + c + " " + str(letters.index(c) +1))
-            count += letters.index(c) +1
-            break
+    lines.insert(0, line)
+    if len(lines) == 3:
+        for c in lines[0]:
+            if lines[1].find(c) != -1 and lines[2].find(c) != -1:
+                count += letters.index(c) +1
+                break
+        lines = []
 print(count)
+
+
+#Part 1 Solution
+# file = open("Day3Input.txt")
+# count = 0
+# letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# for line in file:
+#     mid = int(len(line)/2)
+#     front = line[0:mid]
+#     back = line[mid:-1]
+#     for c in front:
+#         if back.find(c) != -1:
+#             count += letters.index(c) +1
+#             break
+# print(count)
