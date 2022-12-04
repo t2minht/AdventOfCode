c5da4aadf038c57f3e3cdcc813d5119eb60bde93
t2minht <tuongtran0240@gmail.com>
2022-12-04 05:31:10 -0600

    Day 4 Part 1 Completed

---
+++ Day4/Day4.py
@@ -0,0 +1,9 @@
+file = open("Day4Input.txt")
+count = 0
+for line in file:
+    nums = line.replace("-", ",").replace("\n", "").split(",")
+    print(nums)
+    if((int(nums[0]) <= int(nums[2]) and int(nums[1]) >= int(nums[3])) or (int(nums[2]) <= int(nums[0]) and int(nums[3]) >= int(nums[1]))):
+        count+= 1
+print(count)
+        
