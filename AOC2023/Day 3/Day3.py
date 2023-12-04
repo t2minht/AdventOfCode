b55c25d9ea7986889ae6b8fec65c42fb5ec8e844
t2minht <tuongtran0240@gmail.com>
2023-12-03 22:26:45 -0600

    So tired rn, i'll do it tmrw

--- AOC2023/Day 3/Day3.py
+++ AOC2023/Day 3/Day3.py
@@ -1,3 +1,4 @@
+import re
 '''
 p1 sudo code
 We only care about numbers attached to symbols
@@ -8,3 +9,51 @@ If numberic, check values j-1 and j+1 repeatedly to find the full number
 * kinda 2 pointer-y?
 once you have full number, add to sum
 '''
+# def findNumber(line, j):
+#     a = j -1
+#     b = j +1
+#     print("|123456789|123456789|123456789|123456789|123456789|123456789|123456789|123456789|123456789|123456789|123456789|123456789|123456789|123456789|123456789|123456789")
+#     print(line.rstrip())
+#     print(j)
+#     print(line[j:j+1:])
+#     if line[j:j+1:].isnumeric():
+#         if a < 0 or b > len(line):
+#             return -2
+#         if line[a:a+1:].isnumeric() and line[b:b+1:].isnumeric():
+#             return  
+#         if line[a:a+1:].isnumeric():
+#             return findNumber(line, a)
+#         return findNumber(line, b)
+#     return -1
+
+
+file = open("test.txt")
+storedFile = []
+for line in file:
+    storedFile.append(line)
+for i in range(len(storedFile)):
+    index = 0
+    line = storedFile[i].rstrip()
+    symbolList = re.findall("[^0-9.]", line)
+    # print(line)
+    for symbol in symbolList:
+        print(symbol)
+        j = line[index::].find(symbol) + index
+        index = j + 1
+        if(i != 0):
+            if storedFile[i-1][j:j+1:] != ".":
+                print(re.findall('\d+', storedFile[i-1][j-1:j+1:]))
+            elif storedFile[i-1][-1:j:] != ".":
+                num = re.findall('\d+', storedFile[i-1][j-3:j:])
+                print(num[len(num)-1])
+        #     print(re.findall('\d+', storedFile[i-1][j-3:j+4:]))
+        #     print(re.findall('\d+', line[j-3:j+4:]))
+        # if(i != len(storedFile)-1):
+        #     print(re.findall('\d+', storedFile[i+1][j-3:j+4:]))
+    index = 0
+
+
+        
+
+    # while re.search("[^0-9.]", line[index]):
+    #     symbol = line.find("")
