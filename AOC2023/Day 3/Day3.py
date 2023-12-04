6fe8d1821d3344aa1043c74cb8e5cac9ece3e5ba
t2minht <tuongtran0240@gmail.com>
2023-12-04 10:54:09 -0600

    Got it lol

--- AOC2023/Day 3/Day3.py
+++ AOC2023/Day 3/Day3.py
@@ -9,51 +9,161 @@ If numberic, check values j-1 and j+1 repeatedly to find the full number
 * kinda 2 pointer-y?
 once you have full number, add to sum
 '''
-# def findNumber(line, j):
-#     a = j -1
-#     b = j +1
-#     print("|123456789|123456789|123456789|123456789|123456789|123456789|123456789|123456789|123456789|123456789|123456789|123456789|123456789|123456789|123456789|123456789")
-#     print(line.rstrip())
-#     print(j)
-#     print(line[j:j+1:])
-#     if line[j:j+1:].isnumeric():
-#         if a < 0 or b > len(line):
-#             return -2
-#         if line[a:a+1:].isnumeric() and line[b:b+1:].isnumeric():
-#             return  
-#         if line[a:a+1:].isnumeric():
-#             return findNumber(line, a)
-#         return findNumber(line, b)
-#     return -1
+def findNumber(line, j, layer):
+    partNumbers = []
+    if layer == "middle":
+        index = j-1
+        num = ''
+        while line[index: index+1:].isnumeric() and index > -1:
+            num = line[index: index+1:] + num
+            index -= 1
+        if num != '':
+            # print(num)
+            partNumbers.append(int(num))
+        index = j+1
+        num = ''
+        while line[index: index+1:].isnumeric() and index < len(line):
+            num = num + line[index: index+1:]
+            index += 1
+        if num != '':
+            # print(num)
+            partNumbers.append(int(num))
+    else:
+        if line[j: j+1:].isnumeric():
+            index = j-1
+            num = '' + line[j: j+1:]
+            while line[index: index+1:].isnumeric() and index > -1:
+                num = line[index: index+1:] + num
+                index -= 1
+            index = j+1
+            while line[index: index+1:].isnumeric() and index < len(line):
+                num = num + line[index: index+1:]
+                index += 1
+            if num != '':
+                # print(num)
+                partNumbers.append(int(num))
+        else:
+            index = j-1
+            num = ''
+            while line[index: index+1:].isnumeric() and index > -1:
+                num = line[index: index+1:] + num
+                index -= 1
+            if num != '':
+                # print(num)
+                partNumbers.append(int(num))
+            index = j+1
+            num = ''
+            while line[index: index+1:].isnumeric() and index < len(line):
+                num = num + line[index: index+1:]
+                index += 1
+            if num != '':
+                # print(num)
+                partNumbers.append(int(num))
+    return partNumbers
 
 
-file = open("test.txt")
+file = open("Day3Input.txt")
 storedFile = []
+sum = 0
 for line in file:
     storedFile.append(line)
 for i in range(len(storedFile)):
     index = 0
     line = storedFile[i].rstrip()
     symbolList = re.findall("[^0-9.]", line)
-    # print(line)
     for symbol in symbolList:
-        print(symbol)
         j = line[index::].find(symbol) + index
         index = j + 1
-        if(i != 0):
-            if storedFile[i-1][j:j+1:] != ".":
-                print(re.findall('\d+', storedFile[i-1][j-1:j+1:]))
-            elif storedFile[i-1][-1:j:] != ".":
-                num = re.findall('\d+', storedFile[i-1][j-3:j:])
-                print(num[len(num)-1])
-        #     print(re.findall('\d+', storedFile[i-1][j-3:j+4:]))
-        #     print(re.findall('\d+', line[j-3:j+4:]))
-        # if(i != len(storedFile)-1):
-        #     print(re.findall('\d+', storedFile[i+1][j-3:j+4:]))
-    index = 0
-
-
         
+        partNumbers = []
+        partNumbers.extend(findNumber(line, j, "middle"))
+        if i > 0:
+            partNumbers.extend(findNumber(storedFile[i-1], j, ""))
+        if(i != len(storedFile)-1):
+            partNumbers.extend(findNumber(storedFile[i+1], j, ""))
+        # print(partNumbers)
+        if len(partNumbers) < 2:
+            continue
+        product = 1
+        for x in partNumbers:
+            product *= x
+        sum += product
+    index = 0
+print(sum)
 
-    # while re.search("[^0-9.]", line[index]):
-    #     symbol = line.find("")
+
+# Part 1
+'''
+def findNumber(line, j, layer):
+    sum = 0
+    if layer == "middle":
+        index = j-1
+        num = ''
+        while line[index: index+1:].isnumeric() and index > -1:
+            num = line[index: index+1:] + num
+            index -= 1
+        if num != '':
+            print(num)
+            sum += int(num)
+        index = j+1
+        num = ''
+        while line[index: index+1:].isnumeric() and index < len(line):
+            num = num + line[index: index+1:]
+            index += 1
+        if num != '':
+            print(num)
+            sum += int(num)
+    else:
+        if line[j: j+1:].isnumeric():
+            index = j-1
+            num = '' + line[j: j+1:]
+            while line[index: index+1:].isnumeric() and index > -1:
+                num = line[index: index+1:] + num
+                index -= 1
+            index = j+1
+            while line[index: index+1:].isnumeric() and index < len(line):
+                num = num + line[index: index+1:]
+                index += 1
+            if num != '':
+                print(num)
+                sum += int(num)
+        else:
+            index = j-1
+            num = ''
+            while line[index: index+1:].isnumeric() and index > -1:
+                num = line[index: index+1:] + num
+                index -= 1
+            if num != '':
+                print(num)
+                sum += int(num)
+            index = j+1
+            num = ''
+            while line[index: index+1:].isnumeric() and index < len(line):
+                num = num + line[index: index+1:]
+                index += 1
+            if num != '':
+                print(num)
+                sum += int(num)
+    return sum
+
+file = open("Day3Input.txt")
+storedFile = []
+sum = 0
+for line in file:
+    storedFile.append(line)
+for i in range(len(storedFile)):
+    index = 0
+    line = storedFile[i].rstrip()
+    symbolList = re.findall("[^0-9.]", line)
+    for symbol in symbolList:
+        j = line[index::].find(symbol) + index
+        index = j + 1
+        
+        sum += findNumber(line, j, "middle")
+        if i > 0:
+            sum += findNumber(storedFile[i-1], j, "")
+        if(i != len(storedFile)-1):
+            sum += findNumber(storedFile[i+1], j, "")
+    index = 0
+print(sum)
+'''
