9f186f9949acfd23765aff24993d00aa67d90485
t2minht <tuongtran0240@gmail.com>
2023-12-06 12:09:04 -0600

    Tried a recursive method out of curiosity, but it wasn't faster

--- AOC2023/Day 6/Day6.py
+++ AOC2023/Day 6/Day6.py
@@ -1,21 +1,57 @@
-import re
-time = 0
+"""
+def recursiveSearch(currTime, maxTime, distanceToBeat, incVal):
+    # print(currTime)
+    if(currTime * (maxTime - currTime)) < distanceToBeat:
+        return 0
+    # print("distance: " + str(currTime * (maxTime - currTime)))
+    count=1
+    count += recursiveSearch(currTime + incVal, maxTime, distanceToBeat, incVal)
+
+    return count
+
+
+import time, sys
+tme = 0
 distance = 0
 with open("Day6Input.txt") as file:
     temp = file.readline()
-    time = int(temp[temp.find(":")+1::].strip().replace(" ", ""))
+    tme = int(temp[temp.find(":")+1::].strip().replace(" ", ""))
+    temp = file.readline()
+    distance = int(temp[temp.find(":")+1::].strip().replace(" ", ""))
+print("maxTime: " + str(tme))
+print("distance: " + str(distance))
+sys.setrecursionlimit(50000000)
+t0 = time.time()
+numberOfPossibleWins = recursiveSearch(int(tme/2), tme, distance, 1) + recursiveSearch(int(tme/2), tme, distance, -1) -1
+t1 = time.time()
+print(numberOfPossibleWins)
+print(t1 - t0)
+
+#p2 - basically trimmed version of p1, but slow for big input
+# 11.719378232955933 seconds
+"""
+import time
+tme = 0
+distance = 0
+with open("Day6Input.txt") as file:
+    temp = file.readline()
+    tme = int(temp[temp.find(":")+1::].strip().replace(" ", ""))
     temp = file.readline()
     distance = int(temp[temp.find(":")+1::].strip().replace(" ", ""))
 numberOfPossibleWins = 0
-for j in range(time):
+t0 = time.time()
+for j in range(tme):
     # speed = j
     # remaining time = time - j
-    distanceTravelled = j * (time - j)
+    distanceTravelled = j * (tme - j)
     if distanceTravelled < distance:
         continue
     if distanceTravelled > distance:
         numberOfPossibleWins+= 1
+t1 = time.time()
 print(numberOfPossibleWins)
+print(t1 - t0)
+# """
 
 
 #p1
